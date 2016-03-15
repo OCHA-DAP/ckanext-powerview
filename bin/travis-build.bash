4fa2f6ac3b430ff68862a901d9ce2d7b01bc13ce
Brook Elgie <specialbrew@gmail.com>
2016-03-15 17:16:57 +0000

    Set up for travis

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,9 +10,7 @@ sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
-echo "CKAN branch: $latest_ckan_release_branch"
-git checkout $latest_ckan_release_branch
+git checkout $CKAN_BRANCH
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
@@ -29,7 +27,9 @@ cd -
 
 echo "Installing ckanext-powerview and its requirements..."
 python setup.py develop
+pip install -r requirements.txt
 pip install -r dev-requirements.txt
+
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
