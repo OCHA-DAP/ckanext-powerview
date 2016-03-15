acf450da3a805cf6b6731b830602a8caa45e8bee
Brook Elgie <specialbrew@gmail.com>
2016-03-15 16:35:26 +0000

    Test private default value

--- ckanext/powerview/tests/action/test_create.py
+++ ckanext/powerview/tests/action/test_create.py
@@ -35,3 +35,20 @@ class TestCreatePowerView(TestBase):
         nosetools.assert_true(powerview_result['view_type'] == 'my-view-type')
         nosetools.assert_true(isinstance(powerview_result['config'], dict))
         nosetools.assert_true(powerview_result['private'])
+        # created_by field auto-populated
+        nosetools.assert_equal(powerview_result['created_by'], sysadmin['id'])
+
+    def test_powerview_create_private_defaults_to_false(self):
+        '''private will default to False when not specified.'''
+
+        sysadmin = Sysadmin()
+
+        create_dict = self._make_create_data_dict()
+        del create_dict['private']
+
+        powerview_result = toolkit.get_action('powerview_create')(
+            context={'user': sysadmin['name']},
+            data_dict=create_dict
+        )
+
+        nosetools.assert_false(powerview_result['private'])
