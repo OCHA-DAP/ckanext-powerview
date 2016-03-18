b2c55e4605cf837006bc8be6d65d8bac515c4498
Brook Elgie <specialbrew@gmail.com>
2016-03-18 10:56:34 +0000

    Honor 'private' state on powerview show.
    Only the user defined by powerview.created_by (and sysadmins) can pass
    auth for powerview show.

--- ckanext/powerview/logic/auth.py
+++ ckanext/powerview/logic/auth.py
@@ -1,5 +1,11 @@
 import ckan.plugins.toolkit as toolkit
 
+import ckan.model as model
+
+from ckantoolkit import _
+
+from ckanext.powerview.model import PowerView
+
 
 def create(context, data_dict):
     '''Create a PowerView.
@@ -27,5 +33,42 @@ def update(context, data_dict):
 
 @toolkit.auth_allow_anonymous_access
 def show(context, data_dict):
-    '''All users can access a PowerView show.'''
-    return {'success': True}
+    '''All users can view public PowerViews.'''
+
+    powerview = PowerView.get(id=data_dict['id'])
+
+    # If public, just allow
+    if powerview and not powerview.private:
+        return {'success': True}
+
+    # So we haven't got a powerview or it's private...
+
+    user = context.get('user')
+
+    if powerview and user:
+        user_obj = model.User.get(user)
+
+        authorized = _user_has_permission_for_powerview(user_obj, powerview)
+
+        if authorized:
+            return {'success': True}
+        else:
+            return {
+                'success': False,
+                'msg': _('User {0} not authorized to read powerview {1}'
+                         .format(user, powerview.id))}
+    else:
+        return {'success': False}
+
+
+def _user_has_permission_for_powerview(user, powerview):
+    '''Return True if user has permission to view powerview.
+
+    If private, only the creator can view the powerview, but may include
+    organization checks here in the future.
+    '''
+
+    if not powerview.private or powerview.created_by == user.id:
+        return True
+    else:
+        return False
--- ckanext/powerview/tests/test_auth.py
+++ ckanext/powerview/tests/test_auth.py
@@ -1,9 +1,27 @@
 from nose import tools as nosetools
 
+import ckan.plugins.toolkit as toolkit
+
 from ckantoolkit.tests import helpers, factories
 from ckantoolkit import NotAuthorized
 
 from ckanext.powerview.tests import TestBase
+
+
+def _make_powerview(user, private='yes'):
+    '''Make a powerview and return the resulting data_dict.'''
+    data_dict = {
+        'title': 'Title',
+        'description': 'My short description.',
+        'view_type': 'my-view-type',
+        'config': '{"my":"json"}',
+        'private': private
+    }
+
+    return toolkit.get_action('powerview_create')(
+        context={'user': user['name']},
+        data_dict=data_dict
+    )
 
 
 class TestPowerViewCreateAuth(TestBase):
@@ -104,23 +122,71 @@ class TestPowerViewShowAuth(TestBase):
         '''
         a_sysadmin = factories.Sysadmin()
         context = {'user': a_sysadmin['name'], 'model': None}
+        powerview = _make_powerview(a_sysadmin)
         nosetools.assert_true(helpers.call_auth('ckanext_powerview_show',
-                                                context=context))
+                                                context=context,
+                                                data_dict=powerview))
 
     def test_powerview_show_normal_user(self):
         '''
         Calling powerview show with normal logged in user doesn't raise
-        NotAuthorized.
+        NotAuthorized for public powerview.
         '''
         a_user = factories.User()
+        a_sysadmin = factories.Sysadmin()
+        powerview = _make_powerview(a_sysadmin, private='no')
+
         context = {'user': a_user['name'], 'model': None}
         nosetools.assert_true(helpers.call_auth('ckanext_powerview_show',
-                                                context=context))
+                                                context=context,
+                                                id=powerview['id']))
 
     def test_powerview_show_no_user(self):
         '''
-        Calling powerview show with no user doesn't raise NotAuthorized.
+        Calling powerview show with no user doesn't raise NotAuthorized for
+        public powerview.
         '''
+        a_sysadmin = factories.Sysadmin()
+        powerview = _make_powerview(a_sysadmin, private='no')
+
         context = {'user': None, 'model': None}
         nosetools.assert_true(helpers.call_auth('ckanext_powerview_show',
-                                                context=context))
+                                                context=context,
+                                                id=powerview['id']))
+
+    def test_powerview_show_normal_user_private(self):
+        '''
+        Calling powerview show with normal logged in user raises NotAuthorized
+        for private powerview.
+        '''
+        a_user = factories.User()
+        a_sysadmin = factories.Sysadmin()
+        powerview = _make_powerview(a_sysadmin, private='yes')
+
+        context = {'user': a_user['name'], 'model': None}
+        nosetools.assert_raises(NotAuthorized, helpers.call_auth,
+                                'ckanext_powerview_show',
+                                context=context, id=powerview['id'])
+
+    def test_powerview_show_no_user_private(self):
+        '''
+        Calling powerview show with no user raises NotAuthorized for private
+        powerview.
+        '''
+        a_sysadmin = factories.Sysadmin()
+        powerview = _make_powerview(a_sysadmin, private='yes')
+
+        context = {'user': None, 'model': None}
+        nosetools.assert_raises(NotAuthorized, helpers.call_auth,
+                                'ckanext_powerview_show',
+                                context=context, id=powerview['id'])
+
+    def test_powerview_show_nonexisting_powerview(self):
+        '''
+        Calling powerview show with a nonexisting powerview id should raise
+        NotAuthorized.
+        '''
+        context = {'user': None, 'model': None}
+        nosetools.assert_raises(NotAuthorized, helpers.call_auth,
+                                'ckanext_powerview_show',
+                                context=context, id='id-not-here')
